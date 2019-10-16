6c89d78994e0f8767ab767ced3ad9515a5ec1458
Marceloni Lima <markill123@gmail.com>
2019-10-16 13:22:59 -0300

    Dice app implemented.

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import RollDice from './components/rolldice.component';
+
+function App() {
+  return (
+    <div className="App">
+      <RollDice />
+    </div>
+  );
+}
+
+export default App;
